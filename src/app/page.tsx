9b45fa37f0ffe1da65b15f435b30187e433ed476
Naim AK <149870242+naimak659@users.noreply.github.com>
2025-05-27 22:14:30 +0600

    Refactor form layout and validation handling in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,63 +36,69 @@ export default function Home() {
     const { name, value } = e.target;
     setStoreInfo({ ...storeInfo, [name]: value });
   };
+
+  console.log("storeInfo", storeInfo);
   return (
     <>
-      <div className="min-h-screen bg-gray-200 flex items-center justify-center">
-        <div className="bg-white p-8 rounded-md shadow">
-          <div>
-            <h1 className="text-3xl font-bold mb-4">Create a store</h1>
-            <p>Add your basic store information and complete the setup</p>
-          </div>
-          <hr className="my-4 bg-black opacity-30" />
-
-          {/* Form */}
-          <div className="space-y-8">
-            {/* store name */}
-            <div className="grid grid-cols-2 items-center gap-2">
-              <FormLabelInfo
-                title="Give your online store a name"
-                icon={<MonitorIcon color="blue" size={24} weight="bold" />}
-                description="A great store name is a big part of your success. Make sure it aligns with your brand and products."
-              />
-              <input
-                type="text"
-                name="storeName"
-                placeholder="How'd you like to call your store?"
-                className={`border px-2 py-3.5 rounded-md focus-visible:outline-none ${
-                  validation.storeName ? "border-red-500" : "border-gray-300"
-                }`}
-              />
-              <FormValidationMessage message={validation.storeName} />
+      <div className="bg-gray-200">
+        <div className="container min-h-screen mx-auto flex items-center justify-center">
+          <div className="bg-white p-8 rounded-md shadow">
+            <div>
+              <h1 className="text-3xl font-bold mb-4">Create a store</h1>
+              <p>Add your basic store information and complete the setup</p>
             </div>
+            <hr className="my-4 bg-black opacity-30" />
 
-            {/* store subdomain */}
-            <div className="grid grid-cols-2 items-center gap-2 ">
-              <FormLabelInfo
-                title="Your online store subdomain"
-                icon={
-                  <GlobeHemisphereWestIcon
-                    color="blue"
-                    size={24}
-                    weight="fill"
-                  />
-                }
-                description="A SEO-friendly store name is a crucial part of your success. Make sure it aligns with your brand and products."
-              />
-              <div
-                className={`relative  flex border px-2 py-3.5 w-full rounded-md ${
-                  validation.storeName ? "border-red-500" : "border-gray-300"
-                }`}
-              >
+            {/* Form */}
+            <div className="space-y-8">
+              {/* store name */}
+              <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-2">
+                <FormLabelInfo
+                  title="Give your online store a name"
+                  icon={<MonitorIcon color="blue" size={24} weight="bold" />}
+                  description="A great store name is a big part of your success. Make sure it aligns with your brand and products."
+                />
                 <input
                   type="text"
                   name="storeName"
                   placeholder="How'd you like to call your store?"
-                  className={`w-[90%] focus-visible:outline-none`}
+                  className={`border px-2 py-3.5 rounded-md focus-visible:outline-none ${
+                    validation.storeName ? "border-red-500" : "border-gray-300"
+                  }`}
+                  onChange={handleOnChange}
                 />
-                <span className="text-gray-500 ">.expressitbd.com</span>
+                <FormValidationMessage message={validation.storeName} />
               </div>
-              <FormValidationMessage message={validation.storeName} />
+
+              {/* store subdomain */}
+              <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-2 ">
+                <FormLabelInfo
+                  title="Your online store subdomain"
+                  icon={
+                    <GlobeHemisphereWestIcon
+                      color="blue"
+                      size={24}
+                      weight="fill"
+                    />
+                  }
+                  description="A SEO-friendly store name is a crucial part of your success. Make sure it aligns with your brand and products."
+                />
+                <div
+                  className={`flex items-center border px-2 py-3.5 w-full rounded-md ${
+                    validation.domain ? "border-red-500" : "border-gray-300"
+                  }`}
+                >
+                  <input
+                    type="text"
+                    name="domain"
+                    placeholder="enter your domain name"
+                    className={`w-[90%] focus-visible:outline-none`}
+                    onChange={handleOnChange}
+                  />
+                  <span className="text-gray-500">.expressitbd.com</span>
+                </div>
+                <FormValidationMessage message={validation.domain} />
+              </div>
             </div>
           </div>
         </div>
--- src/components/FormLabelInfo.tsx
+++ src/components/FormLabelInfo.tsx
@@ -8,16 +8,21 @@ type Props = {
 
 const FormLabelInfo = ({ title, description, icon }: Props) => {
   return (
-    <div className="flex items-start gap-4 ">
-      <div>
-        <span>{icon}</span>
+    <>
+      <div className="flex items-start gap-4 ">
+        <div className="hidden md:block">
+          <span>{icon}</span>
+        </div>
+        <div className="space-y-2">
+          <div className="flex items-center gap-2">
+            <span className="block md:hidden">{icon}</span>
+            <h2 className="font-bold">{title}</h2>
+          </div>
+          {/* todo: fix the max width for the description */}
+          <p className="text-black max-w-[500px]">{description}</p>
+        </div>
       </div>
-      <div className="space-y-2">
-        <h2 className="font-bold">{title}</h2>
-        {/* todo: fix the max width for the description */}
-        <p className="text-black max-w-[500px]">{description}</p>
-      </div>
-    </div>
+    </>
   );
 };
 
